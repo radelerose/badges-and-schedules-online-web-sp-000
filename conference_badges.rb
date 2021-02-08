69286882527fa97ea3bc0b180031ec8d92bd13a2
Radele Rose <radele.rose@gmail.com>
2021-02-08 05:11:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,5 +7,5 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  "Hello, #{attendees}! You'll be assigned to room #{each_with_index}!"  
+  "Hello, #{attendees}! You'll be assigned to room #{each_with_index}!"
 end
